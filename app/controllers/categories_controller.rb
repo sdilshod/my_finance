2ee7da336314ff64120f0fc20094c54e3add1539
sdilshod <samatov.dilshod@gmail.com>
2014-12-21 22:06:27 +0500

    [add] category and subcategory views and change top menu

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,46 @@
+#TODO use inheretedResources
+class CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.order('name')
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new category_params
+    if @category.save
+      redirect_to categories_path
+    else
+      render new_category_path
+    end
+  end
+
+  def edit
+    @category = Category.find params[:id]
+  end
+
+  def update
+    @category = Category.find params[:id]
+    if @category.update category_params
+      redirect_to categories_path
+    else
+      render edit_category_path
+    end
+
+  end
+
+  def destroy
+    @category = Category.find params[:id]
+    @category.destroy
+    redirect_to categories_path
+  end
+
+private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
