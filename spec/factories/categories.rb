0ffae779637dfca37ec81fc748a4716ff35b4481
sdilshod <samatov.dilshod@gmail.com>
2014-12-22 23:27:40 +0500

    [add] data to select tags

---
+++ spec/factories/categories.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :category_road, class: Category do
+    name 'Дорога'
+  end
+
+  factory :category_house, class: Category do
+    name 'Дом'
+  end
+
+end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Category, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'scopes' do
+
+    before :all do
+      @cat_road = create(:category_road)
+      @cat_house = create(:category_house)
+    end
+
+    it '.get_select_data' do
+      arr = Category.get_select_data
+      expect(arr[0].size).to eql(2)
+    end
+
+  end
+
 end
