5eba63945f7de5a280ce0c5ed3a21e974e312751
sdilshod <samatov.dilshod@gmail.com>
2015-01-05 22:40:15 +0500

    [add] pagination and some chore of code

--- app/assets/javascripts/operations.js
+++ app/assets/javascripts/operations.js
@@ -1,3 +1,4 @@
+//TODO refactor with joining codes
 $(document).ready(function(){
   $(document).on('change', '#operation_category_id', function(){
     var current_category_value = this.value;
@@ -11,4 +12,18 @@ $(document).ready(function(){
         $('#operation_subcategory_id').animate({opacity: 1}, 1000);
       });
   });
+
+  $(document).on('change', '#filter_category', function(){
+    var current_category_value = this.value;
+    $.get('/operations/fill_subcategory.json', {category_id: current_category_value})
+      .done(function(data){
+        $('#filter_subcategory').html('');
+        $.each(data.subcategories, function(index, item){
+          $('#filter_subcategory').append('<option value = '+item[1]+'>'+item[0]+'</option>');
+        });
+        $('#filter_subcategory').animate({opacity: .5}, 600);
+        $('#filter_subcategory').animate({opacity: 1}, 1000);
+      });
+  });
+
 });
