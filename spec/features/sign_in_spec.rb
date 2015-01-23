f13c6e03820aa1c7cf1d45f2ec20f1a593a866d4
sdilshod <samatov.dilshod@gmail.com>
2015-01-23 22:16:08 +0500

    [add\fix\change] pnotify, some fix and changes, mark up sign_up page

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -8,14 +8,14 @@ describe 'The sign in process' do
 
   it 'sign in user' do
     visit '/'
-    expect(page).not_to have_content 'Операции'
+    expect(page.html).not_to include('<li>Операции</li>')
     expect(page).to have_content 'Войти'
     visit '/users/sign_in'
     within('form') do
       fill_in 'user_email', with: @user.email
       fill_in 'user_password', with: @user.password
     end
-    click_button 'Log in'
+    click_button 'Войти'
     expect(current_path).to eq(root_path)
     expect(page).to have_content 'Операции Категории Субкатегории'
   end
